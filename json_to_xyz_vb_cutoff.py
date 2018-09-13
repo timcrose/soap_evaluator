a0d3cab41effac9ad084df95ed92048e84864342
timcrose <trose@andrew.cmu.edu>
2018-09-13 12:29:42 -0400

    added json_to_xyz_vb_cutoff.py which converts a folder of json structures to a single concatenated xyz file containing relative energies in the comment line (relative to the min energy in the folder of jsons)

---
+++ json_to_xyz_vb_cutoff.py
@@ -0,0 +1,58 @@
+import os, glob, json, shutil
+
+input_str = 'jsons_no_dups'
+scpt_dir = os.path.dirname(os.path.abspath(__file__))
+input_dir = os.path.join(scpt_dir, input_str)
+output_dir = os.path.join(scpt_dir, 'training_xyz')
+
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+
+outfile_name = os.path.join(output_dir, input_str + '.xyz')
+
+#Energy name in jsons
+energy_name = 'energy'
+#Structs with energies greater than energy_cutoff will not be included
+# in the final xyz file.
+energy_cutoff = 0
+
+
+#clean for new run
+if os.path.isfile(outfile_name):
+    os.remove(outfile_name)
+    
+data_files = glob.glob(input_dir + '/*')
+
+def get_GM_energy(data_files):
+    GM_energy = 0
+    for data_file in data_files:
+        data = read_json(data_file)
+        en= float(data['properties'][energy_name])
+        if en < GM_energy:
+            GM_energy = en
+
+    return GM_energy
+
+def read_json(file_name):
+    with open (file_name) as in_file:
+        data = json.load(in_file)
+        return data
+
+#Out of all the jsons, this is the lowest energy
+GM_energy = get_GM_energy(data_files)
+        
+for data_file in data_files:
+    data = read_json(data_file)
+    en=data['properties'][energy_name]
+    if (en < energy_cutoff) :
+        energy_str = 'energy=' + str(en - GM_energy)
+        N_atoms = str(len(data['geometry']))
+        lattice_vecs_str = 'Lattice=' + '"' + str(data['properties']['lattice_vector_a'][0]) + ' ' + str(data['properties']['lattice_vector_a'][1]) + ' ' + str(data['properties']['lattice_vector_a'][2]) + ' ' + str(data['properties']['lattice_vector_b'][0]) + ' ' + str(data['properties']['lattice_vector_b'][1]) + ' ' + str(data['properties']['lattice_vector_b'][2]) + ' ' + str(data['properties']['lattice_vector_c'][0]) + ' ' + str(data['properties']['lattice_vector_c'][1]) + ' ' + str(data['properties']['lattice_vector_c'][2]) + '"'
+        properties_str = 'Properties=species:S:1:pos:R:3'
+        comment_line = lattice_vecs_str + ' ' + energy_str + ' ' + properties_str
+        outfile_name = os.path.join(output_dir, input_str + '.xyz')
+        with open(outfile_name, 'a') as f:
+            f.write(N_atoms + '\n')
+            f.write(comment_line + '\n')
+            for xyzS in data['geometry']:
+                f.write(str(xyzS[3]) + ' ' + str(xyzS[0]) + ' ' + str(xyzS[1]) + ' ' + str(xyzS[2]) + '\n')
